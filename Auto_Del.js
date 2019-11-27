36e39b7bcd47e025fe041d56349574ddde9e86c6
CharlesW1970 <41504031+CharlesW1970@users.noreply.github.com>
2019-11-27 19:22:19 +0800

    Create Auto_Del.js

---
+++ Auto_Del.js
@@ -0,0 +1,25 @@
+var delay = 1000;  //删除与确认操作以1000ms为周期循环进行
+function del() {
+    try {
+        document.querySelector('.app_canvas_frame').contentDocument.querySelector('.del_btn').click();
+        setTimeout("yes()", delay);
+    }
+    catch (err) {
+        next_page();  //删除本页所有说说后自动翻页
+    }
+}
+function yes() {
+    document.querySelector('.qz_dialog_layer_btn').click();
+    setTimeout("del()", delay);
+}
+function next_page() {
+    document.querySelector('.app_canvas_frame').contentWindow.document.querySelectorAll('.mod_pagenav_main>a').forEach(el => {
+        if (el.title === '下一页') {
+            el.click();
+            setTimeout("del()", 5000);  
+        }
+    })
+}
+while (1) {
+    del();
+}
